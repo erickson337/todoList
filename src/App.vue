8cb979e15656dbf91eb6b37669b71759d52e2efe
erickson <ericksonicodemos@outlook.com>
2020-08-25 18:14:48 -0300

    Função informativa implementada.

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,29 @@
     <nav class="navbar navbar navbar-dark bg-dark">
       <div id="header"> TodoList </div>
     </nav>
-    <div class="row p-3 ml-3">
-      <div class="col-sm-8 shadow p-2 mb-5 bg-white rounded">
+    <div class="row p-3 ml-3 justify-content-between">
+      <div class="col-8 shadow p-2 mb-5 bg-white rounded">
         <div class="card">
           <div class="card-body overflow-auto">
             <CheckBox :data="data" id="checkbox"/>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 p-1">
+        <div class="modal-helper p-1 col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-11 col-sm-11 col-md-11 col-lg-11 border rounded bg-white shadow-border m-2" v-if="toggle_button">
+                <div v-for="message in messages" :key="message">
+                  <div class="float-left text-primary"><small><b>{{message}}</b></small></div><br>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div>
+                  <button type="button" style="border-radius: 50%;" @click="toggle_button = !toggle_button" class="btn btn-primary m-2 float-right"><v-icon :name="`${toggle_button ? 'times' : 'question'}`"/></button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -46,11 +64,21 @@ export default {
   data () {
     return {
       data: [],
-      instance_data: {}
+      instance_data: {},
+      toggle_button: false,
+      messages: [
+        '- Press enter key for add item;',
+        '- Press shift key for add sub item;',
+        '- Press delete key for remove item or sub item;',
+        '* For delete and add sub item specific, click in a item or sub item.'
+      ]
     }
   },
   methods: {
     generateHashID,
+    teste () {
+      console.count('aqui')
+    },
     deletePositionArray () {
       let find_data = (data, wanted) => {
         data.forEach((elem, index) => {
